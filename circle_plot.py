760fa0a629250b901563c131a0765e385e292e55
davimgeo <156332851+davimgeo@users.noreply.github.com>
2024-04-13 00:42:51 -0300

    Update circle_plot.py

--- circle_plot.py
+++ circle_plot.py
@@ -1,21 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-path = r"/home/malum/Desktop/coding_tests/data/"
+path = r"/home/malum/Desktop/coding_tests/circle_exercise/data/"
 
-y_path = path + "y_vector.txt"
-x_path = path + "x_vector.txt"
-
-y_vector = np.loadtxt(y_path)
-x_vector = np.loadtxt(x_path)
+y_vector = np.loadtxt(path + "y_vector.txt")
+x_vector = np.loadtxt(path + "x_vector.txt")
 
 fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6,6))
 
 ax.plot(x_vector, y_vector, 'k')
-ax.plot(x_vector, -y_vector, 'k')
 
 fig.tight_layout()
 plt.grid(True)
 plt.show()
-
-#testando
